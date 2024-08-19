9cc50c2d00ade746cd6170b4e05cad990b576ad6
Alexsandro <jalexsandro2005@gmail.com>
2024-08-19 11:11:16 -0300

    removed telescope and added mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,35 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    }, 
+
+    config = function()
+        require("mason").setup({
+
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
+
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "clangd",
+                "gopls",
+            },
+            automatic_installation = true,
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "clang-format",
+                "golines",
+            },
+        })
+    end
+}
